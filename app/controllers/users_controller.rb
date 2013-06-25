dd77c4a213556bc9c8336e764e4e61ff858fb11c
travisbrault <brault.travis@gmail.com>
2013-06-25 17:41:32 -0400

    controllers and helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,60 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :admin_user,     only: :destroy
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Account Created!"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Account Updated!"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User Deleted!"
+		redirect_to users_path
+	end
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+		private
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user? (@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
+
 end
